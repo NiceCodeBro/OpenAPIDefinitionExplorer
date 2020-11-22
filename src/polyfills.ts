c511640ca6db2bd58582452865475954c31a4db0
Selim Dursun <m.seldur@gmail.com>
2020-11-22 13:39:01 +0100

    swagger parse and its dependencies are installed

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -57,6 +57,10 @@
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+import * as process from 'process';
+ window['process'] = process;
+ global.Buffer = global.Buffer || require('buffer').Buffer;
+
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
