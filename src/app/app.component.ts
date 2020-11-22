7c8e850fc2de780500a09a86ea972668b58bea19
Selim Dursun <m.seldur@gmail.com>
2020-11-22 13:42:30 +0100

    app component is implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import SwaggerParser from "@apidevtools/swagger-parser";
+//import {api} from './parser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openapi-explorer';
+  validEndPoints = [];
+  @ViewChild('child') childElements; 
+  isExpanded: boolean = false;
+
+  ngOnInit()	{
+   this.myFunc()
+  }
+
+  onClick() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+
+
+  
+  myFunc(): Promise<any> {
+    let parser = new SwaggerParser();
+
+    return parser.dereference("openapi-complex.yaml").then((api)=> {
+      for (let [key, value] of Object.entries(api.paths)) {
+          //key: /random-string -  value: {get : {...}}
+          for (let [key2, value2] of Object.entries(value)) {
+              // key2: get - value2: summary -  value2: responses
+              for (let [key3, value3] of Object.entries(value2['responses'])) {
+                  // key3: 200 (error code) - value3.description - value3.content
+                  // if we can reach this point, content is valid, we can save it into our array
+                  if (value3['content'] && value3['content']['application/json'] && 
+                      key3 && value3['content']['application/json'].schema) { 
+                       // console.log(value3)
+                      var tempObj = { endPoint: key}
+                      tempObj['properties'] = value3['content']['application/json'].schema.properties;
+                  
+                      const properties = value3['content']['application/json'].schema.properties;
+                      for (let [key4, value4] of  Object.entries(properties)) {
+                        tempObj[key4] = value4;
+                      }
+                      this.validEndPoints.push(tempObj);
+                  }
+              }
+          }
+      }
+      console.log(this.validEndPoints);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EndpointPropertiesComponent } from './components/endpoint-properties/endpoint-properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EndpointPropertiesComponent
   ],
   imports: [
     BrowserModule
