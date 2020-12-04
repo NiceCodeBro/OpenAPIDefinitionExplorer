1ae69527389e1dff2959f18ac1659d7e5b64d113
Selim Dursun <m.seldur@gmail.com>
2020-12-04 23:55:12 +0100

    backend: parser api is transported to api folder

--- backend/api/v1.0/index.js
+++ backend/api/v1.0/index.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
- 
-router.get('/', function(req, res, next) {
-  res.send('Hello v1.0 GET API from TechBrij.com');
+const { v4: uuidv4 } = require('uuid');
+
+const express = require('express');
+const router = express.Router();
+const swagger = require('../../services/parserservice/swagger');
+
+/**
+ * This endpoint get a file content as string, create a yaml file and parse it
+ * with openapi swagger parser and send results to client back.
+ */
+router.post('/parser/swaggerparser', (req, res) => {
+
+  const uniqueFilename = `${uuidv4()}.yaml`; //create a unique named file
+  const filePath = `./files/${uniqueFilename}`;
+  const fsPromises = require('fs').promises;
+
+  fsPromises.writeFile(filePath, req.body.filecontent).then(() => {
+    const validEndPoints = swagger.openApiSpecParser(filePath);
+
+    try { //try to delete the created file, since there is no need to have it more.
+      fsPromises.unlink(filePath);
+    }
+    catch(err) { // If an error occurred while deleting files
+      console.error(err);
+    }
+
+    return validEndPoints.then((endPoints) => {
+      return res.status(200).send(JSON.stringify(endPoints))
+    })
+    // an error is occured during parsing.
+    .catch((err)=>{
+      console.error(err);
+      return res.status(400).send(JSON.stringify([]));
+    });
+  })
+  //an error is occured during writting to file
+  .catch((err)=>{
+    console.error(err);
+    return res.status(400).send(JSON.stringify([]));
+  });
 });
- 
- 
+
 module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,5 @@
-const { v4: uuidv4 } = require('uuid');
-
 const express = require('express')
 const cors = require('cors')
-const fs = require('fs');
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -10,34 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(cors()) //cors middleware
 
 app.use(express.json());
-app.use('/api', require('./api'));
-
-app.post('/parseMe', (req, res) => {
-
-  const uniqueFilename = `${uuidv4()}.yaml`
-  const filePath = `./files/${uniqueFilename}`;
-
-  fs.writeFile(filePath, req.body.filecontent, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    var swagger = require('./services/parserservice/swagger');
-    const validEndPoints = swagger.openApiSpecParser(filePath)
-
-    try {
-      //try to delete the created file, since there is no need to have it more.
-      fs.unlinkSync(filePath);
-    }
-    // If an error occurred while deleting files
-    catch(err) {
-      console.error(err);
-    }
-
-    return validEndPoints.then((endPoints) => {
-        return res.status(200).send(JSON.stringify(endPoints))
-    });
-  }); 
-});
+app.use('/api', require('./api')); //interpreting with api folder
 
 app.listen(port, () => {
   console.log(`Openapi Explorer APP is listening at http://localhost:${port}`)
