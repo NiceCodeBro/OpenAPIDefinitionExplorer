0411da545354118e884d7b1929b8e5ed92749035
Selim Dursun <m.seldur@gmail.com>
2020-12-07 00:31:06 +0100

    backend: static files are served with outside

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,13 @@ const port = process.env.PORT || 3000;
 app.use(cors()) //cors middleware
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Openapi Explorer APP!')
+});
+
+app.use('/', express.static(__dirname + '/static'));
+
 app.use('/api', require('./api')); //interpreting with api folder
 
 app.listen(port, () => {
